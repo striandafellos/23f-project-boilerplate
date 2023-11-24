3614c9e6c392f58aa7ec026e379acd576c5897e6
striandafellos <54126557+striandafellos@users.noreply.github.com>
2023-11-24 02:07:42 -0500

    docker testing

--- flask-app/src/__init__.py
+++ flask-app/src/__init__.py
@@ -1,5 +1,6 @@
 # Some set up for the application 
 
+import os
 from flask import Flask
 from flaskext.mysql import MySQL
 
@@ -16,10 +17,10 @@ def create_app():
 
     # these are for the DB object to be able to connect to MySQL. 
     app.config['MYSQL_DATABASE_USER'] = 'root'
-    app.config['MYSQL_DATABASE_PASSWORD'] = open('/secrets/db_root_password.txt').readline().strip()
+    app.config['MYSQL_DATABASE_PASSWORD'] = open(os.path.join("..", "secrets", "db_root_password.txt")).readline().strip()
     app.config['MYSQL_DATABASE_HOST'] = 'db'
     app.config['MYSQL_DATABASE_PORT'] = 3306
-    app.config['MYSQL_DATABASE_DB'] = 'northwind'  # Change this to your DB name
+    app.config['MYSQL_DATABASE_DB'] = 'cool_db'  # Change this to your DB name
 
     # Initialize the database object with the settings above. 
     db.init_app(app)
